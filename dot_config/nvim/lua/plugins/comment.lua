9afe5a8cf183af8f99140f23fad2a81b6b114bf3
Dmitry <inyutin.da@gmail.com>
2023-08-30 15:05:51 +0100

    Improve nvim.comment: lazy load; comment selection; comment text-objects

--- dot_config/nvim/lua/plugins/comment.lua
+++ dot_config/nvim/lua/plugins/comment.lua
@@ -1,20 +1,39 @@
--- TODO: learn how to use text-objects
 --- @type LazyPluginSpec
 local comment_plugin = {
 	'numToStr/Comment.nvim',
 	tag = 'v0.8.0',
 	opts = {
+		padding = true,
+		sticky = true,
 		toggler = {
-			line = '<leader>cc',
-			block = '<leader>cb',
+			line = 'gcc',
+			block = 'gbc',
 		},
-		opleader = {},
-		extra = {},
+		opleader = {
+			line = 'gc',
+			block = 'gb',
+		},
+		---LHS of extra mappings
+		extra = {
+			above = 'gcO',
+			below = 'gco',
+			eol = 'gcA',
+		},
+		mappings = {
+			---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+			basic = true,
+			---Extra mapping; `gco`, `gcO`, `gcA`
+			extra = true,
+		},
 	},
 	lazy = true,
 	keys = {
-		'<leader>cc',
-		'<leader>cb',
+		{ "gc", mode = "v" },
+		{ "gb", mode = "v" },
+		'gc',
+		'gb',
+		'gcc',
+		'gcb',
 	},
 }
 
