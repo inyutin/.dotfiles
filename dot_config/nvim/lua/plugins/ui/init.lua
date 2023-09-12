e9047dd8358bd4eed3dc5ed7827f876bdd7bf6a8
Dmitry <inyutin.da@gmail.com>
2023-09-12 19:41:26 +0100

    nvim: add harpoon

--- dot_config/nvim/lua/plugins/ui/init.lua
+++ dot_config/nvim/lua/plugins/ui/init.lua
@@ -1,5 +1,6 @@
 local bufferline_plugin = require("plugins.ui.bufferline")
 local gitsigns_plugin = require("plugins.ui.gitsigns")
+local harpoon_plugin = require("plugins.ui.harpoon")
 local colorscheme_plugin = require("plugins.ui.kanagawa_colorscheme")
 local lualine_plugin = require("plugins.ui.lualine")
 local nvim_tree_plugin = require("plugins.ui.nvim_tree")
@@ -8,6 +9,7 @@ local nvim_tree_plugin = require("plugins.ui.nvim_tree")
 return {
 	bufferline_plugin,
 	gitsigns_plugin,
+	harpoon_plugin,
 	lualine_plugin,
 	colorscheme_plugin,
 	nvim_tree_plugin,
