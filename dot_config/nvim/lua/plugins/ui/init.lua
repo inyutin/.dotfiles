15f1a84d774b408ff1b8ffbaea31a58a86412f2b
Dmitry <inyutin.da@gmail.com>
2023-09-15 13:21:40 +0100

    nvim: add symbols_outline_plugin

--- dot_config/nvim/lua/plugins/ui/init.lua
+++ dot_config/nvim/lua/plugins/ui/init.lua
@@ -5,6 +5,7 @@ local harpoon_plugin = require("plugins.ui.harpoon")
 local colorscheme_plugin = require("plugins.ui.kanagawa_colorscheme")
 local lualine_plugin = require("plugins.ui.lualine")
 local nvim_tree_plugin = require("plugins.ui.nvim_tree")
+local symbols_outline_plugin = require("plugins.ui.symbols_outline")
 
 --- @type LazyPluginSpec[]
 return {
@@ -15,4 +16,5 @@ return {
 	lualine_plugin,
 	colorscheme_plugin,
 	nvim_tree_plugin,
+	symbols_outline_plugin,
 }
