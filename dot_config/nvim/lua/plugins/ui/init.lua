1a85e08813fe492c3407ac8bcb0a720b1f04e9ca
Dmitry <inyutin.da@gmail.com>
2024-06-16 12:20:42 +0100

    nvim: add undotree plugin

--- dot_config/nvim/lua/plugins/ui/init.lua
+++ dot_config/nvim/lua/plugins/ui/init.lua
@@ -5,6 +5,7 @@ local colorscheme_plugin = require("plugins.ui.kanagawa_colorscheme")
 local lualine_plugin = require("plugins.ui.lualine")
 local nvim_tree_plugin = require("plugins.ui.nvim_tree")
 local outline_plugin = require("plugins.ui.outline")
+local undotree_plugin = require("plugins.ui.undotree")
 local window_picker_plugin = require("plugins.ui.window_picker")
 
 --- @type LazyPluginSpec[]
@@ -16,5 +17,6 @@ return {
   colorscheme_plugin,
   nvim_tree_plugin,
   outline_plugin,
+  undotree_plugin,
   window_picker_plugin,
 }
---
+++ dot_config/nvim/lua/plugins/ui/undotree.lua
@@ -0,0 +1,16 @@
+local toggle_undotree = function()
+  local nvim_tree_api = require('nvim-tree.api')
+  nvim_tree_api.tree.close()
+  vim.cmd("UndotreeToggle")
+end
+
+--- @type LazyPluginSpec
+local undotree_plugin = {
+  "mbbill/undotree",
+  lazy = true,
+  cmd = { "UndotreeToggle" },
+  keys = {
+    { "<leader>U", toggle_undotree, desc = "Toggle undotree" },
+  },
+}
+return undotree_plugin
