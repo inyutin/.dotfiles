692b5ecee302cf9f2f27ae989ba38d069d29d22e
Dmitry <inyutin.da@gmail.com>
2024-06-16 11:43:54 +0100

    nvim: add slighly more commands to gitsigns

--- dot_config/nvim/lua/plugins/ui/gitsigns.lua
+++ dot_config/nvim/lua/plugins/ui/gitsigns.lua
@@ -1,10 +1,20 @@
+-- TODO: use vim-fugitive
+
 local function on_attach(bufnr)
   local gs = package.loaded.gitsigns
 
-  vim.keymap.set('n', '<leader>hs', gs.stage_hunk)
-  vim.keymap.set('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-  vim.keymap.set('n', '<leader>hu', gs.undo_stage_hunk)
-  vim.keymap.set("n", "<leader>tb", gs.toggle_current_line_blame)
+  -- Stage
+  vim.keymap.set('n', '<leader>gs', gs.stage_hunk)
+  vim.keymap.set('v', '<leader>gs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+
+  -- Reset
+  vim.keymap.set('n', '<leader>gr', gs.reset_hunk)
+  vim.keymap.set('v', '<leader>gr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+  vim.keymap.set('n', '<leader>gu', gs.undo_stage_hunk)
+
+  -- Blame
+  vim.keymap.set("n", "<leader>gb", gs.toggle_current_line_blame)
+  vim.keymap.set('n', '<leader>gB', function() gs.blame_line { full = true } end)
 end
 
 --- @type LazyPluginSpec
