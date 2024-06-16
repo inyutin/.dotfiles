3c9f98d2b3c43643ae67bf3b8aeea44cf532ab49
Dmitry <inyutin.da@gmail.com>
2024-06-16 12:07:41 +0100

    nvim: little improvements to telescope

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -8,30 +8,30 @@ local find_lsp_classes = function()
   require('telescope.builtin').lsp_dynamic_workspace_symbols(symbol_opts)
 end
 
--- TODO: set up more staff for telescope
+-- TODO set up https://github.com/nvim-telescope/telescope-smart-history.nvim
+-- TODO set up more telescope extensions
+
 --- @type LazyPluginSpec
 local telescope_plugin = {
   "nvim-telescope/telescope.nvim",
-  dependencies = {
-    {
-      "debugloop/telescope-undo.nvim",
-    },
-  },
   config = function(_, _)
-    require('telescope').setup({
-      extensions = { undo = {} },
+    local telescope = require('telescope')
+    local actions = require('telescope.actions')
+
+    telescope.setup({
       defaults = {
         mappings = {
           n = {
-            ['<c-d>'] = require('telescope.actions').delete_buffer
+            ['<C-d>'] = actions.delete_buffer
           },
           i = {
-            ['<c-d>'] = require('telescope.actions').delete_buffer
+            ['<C-d>'] = actions.delete_buffer,
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
           },
         },
       },
     })
-    require("telescope").load_extension("undo")
 
     local keymaps_opts = { noremap = true, silent = true }
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", keymaps_opts)
@@ -39,8 +39,7 @@ local telescope_plugin = {
     vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", keymaps_opts)
     vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", keymaps_opts)
     vim.keymap.set("n", "<leader>fq", "<cmd>Telescope quickfix<cr>", keymaps_opts)
-    vim.keymap.set("n", "<leader>fh", "<cmd>Telescope quickfixhistory<cr>", keymaps_opts)
-    vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>", keymaps_opts)
+    vim.keymap.set("n", "<leader>fG", "<cmd>Telescope git_status<cr>", keymaps_opts)
     vim.keymap.set("n", "<leader>flc", find_lsp_classes, keymaps_opts)
   end,
 }
