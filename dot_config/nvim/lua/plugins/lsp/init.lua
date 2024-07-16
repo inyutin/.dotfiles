91c18266191e3187b4c55e123c60f6678d4ea2b8
Dmitry Inyutin <inyutin.da@gmail.com>
2024-07-16 17:19:07 +0100

    nvim: silent conform errors

--- dot_config/nvim/lua/plugins/lsp/init.lua
+++ dot_config/nvim/lua/plugins/lsp/init.lua
@@ -43,7 +43,7 @@ local conform_plugin = {
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*",
       callback = function(args)
-        require("conform").format({ bufnr = args.buf })
+        require("conform").format({ bufnr = args.buf, quiet = true})
       end,
     })
   end,
