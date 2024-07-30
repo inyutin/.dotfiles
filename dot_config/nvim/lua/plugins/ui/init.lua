5fcad6cd9e88e151b36ba243a77c1df2e71bc5b0
Dmitrii Iniutin <inyutin.da@gmail.com>
2024-07-30 20:58:33 +0100

    nvim: get rid of stickybuf, 'cause I'm using 0.10 now

--- dot_config/nvim/lua/plugins/ui/init.lua
+++ dot_config/nvim/lua/plugins/ui/init.lua
@@ -5,7 +5,6 @@ local colorscheme_plugin = require("plugins.ui.kanagawa_colorscheme")
 local lualine_plugin = require("plugins.ui.lualine")
 local nvim_tree_plugin = require("plugins.ui.nvim_tree")
 local outline_plugin = require("plugins.ui.outline")
-local stickybuf_plugin = require("plugins.ui.stickybuf")
 local undotree_plugin = require("plugins.ui.undotree")
 local window_picker_plugin = require("plugins.ui.window_picker")
 
@@ -18,7 +17,6 @@ return {
   colorscheme_plugin,
   nvim_tree_plugin,
   outline_plugin,
-  stickybuf_plugin,
   undotree_plugin,
   window_picker_plugin,
 }
