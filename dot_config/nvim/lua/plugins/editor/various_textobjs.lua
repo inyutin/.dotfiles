306f90573772c1035083ffe8724baaba11c8278f
Dmitrii Iniutin <inyutin.da@gmail.com>
2025-05-12 01:02:59 +0100

    Nvim, lint files

--- dot_config/nvim/lua/plugins/editor/various_textobjs.lua
+++ dot_config/nvim/lua/plugins/editor/various_textobjs.lua
@@ -1,12 +1,12 @@
 --- @type LazyPluginSpec
 local various_textobjs_plugin = {
-  "chrisgrieser/nvim-various-textobjs",
-  lazy = false,
-  opts = { useDefaultKeymaps = false },
-  config = function(_, opts)
-    require('various-textobjs').setup(opts)
-    vim.keymap.set({ "o", "x" }, 'iS', "<cmd>lua require('various-textobjs').subword('inner')<CR>")
-    vim.keymap.set({ "o", "x" }, 'aS', "<cmd>lua require('various-textobjs').subword('outer')<CR>")
-  end,
+	"chrisgrieser/nvim-various-textobjs",
+	lazy = false,
+	opts = { keymaps = { useDefault = false } },
+	config = function(_, opts)
+		require("various-textobjs").setup(opts)
+		vim.keymap.set({ "o", "x" }, "iS", "<cmd>lua require('various-textobjs').subword('inner')<CR>")
+		vim.keymap.set({ "o", "x" }, "aS", "<cmd>lua require('various-textobjs').subword('outer')<CR>")
+	end,
 }
 return various_textobjs_plugin
